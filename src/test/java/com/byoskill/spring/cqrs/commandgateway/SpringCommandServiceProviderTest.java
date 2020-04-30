5305ad611cf58e5f4c77c6cf5f0655d74796d339
Sylvain Leroy <sleroy0@gmail.com>
2020-04-30 21:47:17 +0200

    Ignoring resource

--- src/test/java/com/byoskill/spring/cqrs/commandgateway/SpringCommandServiceProviderTest.java
+++ src/test/java/com/byoskill/spring/cqrs/commandgateway/SpringCommandServiceProviderTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
 import com.byoskill.spring.cqrs.commands.CommandServiceSpec;
